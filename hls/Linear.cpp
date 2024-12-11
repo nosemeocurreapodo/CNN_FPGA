c0482da91c47676006d2cb54cab9bb9dff1a6e12
Emanuel <emanueltrabes@gmail.com>
2024-12-11 11:42:20 +0100

    fix: added bias into linear con conv2D layers

--- hls/Linear.cpp
+++ hls/Linear.cpp
@@ -1,12 +1,13 @@
 #include "Linear.h"
 
-int Linear(hls::stream<linear_packet> &input, hls::stream<linear_packet> &output, int &in_size, int &out_size, float weights[LINEAR_MAX_WIDTH])
+int Linear(hls::stream<linear_packet> &input, hls::stream<linear_packet> &output, int &in_size, int &out_size, float weights[LINEAR_MAX_WIDTH], float bias[LINEAR_MAX_WIDTH])
 {
 #pragma HLS INTERFACE axis port = input
 #pragma HLS INTERFACE axis port = output
 #pragma HLS INTERFACE s_axilite port = in_size
 #pragma HLS INTERFACE s_axilite port = out_size
 #pragma HLS INTERFACE s_axilite port = weights
+#pragma HLS INTERFACE s_axilite port = bias
 #pragma HLS INTERFACE s_axilite port = return
 
     linear_data_type output_data[LINEAR_MAX_WIDTH];
@@ -40,7 +41,7 @@ write_output_loop:
     for (int i = 0; i < out_size; i++)
     {
         linear_packet out_packet;
-        out_packet.data = float(output_data[i]);
+        out_packet.data = float(output_data[i] + bias[i]);
         out_packet.keep = -1;
         out_packet.strb = -1;
         if(i == out_size - 1)
