bfa73f545a186b98debeef56dcd324cfe537144c
Emanuel <emanueltrabes@gmail.com>
2024-12-14 06:33:08 +0100

    fix: updated dot ip core

--- hls/dot.cpp
+++ hls/dot.cpp
@@ -11,12 +11,12 @@ int dot(hls::stream<dot_packet> &input1, hls::stream<dot_packet> &input2, float 
 
 dot_data_type res[DOT_PPBUFF_SIZE];
 
+init_loop:
 for(int i = 0; i < DOT_PPBUFF_SIZE; i++)
-
- = dot_data_type(0.0f);
+    res[i] = dot_data_type(0.0f);
 
 main_loop:
-    for (int j = 0; j < in_size; j++)
+    for (int i = 0; i < in_size; i++)
     {
 // #pragma HLS PIPELINE II=1
 #pragma HLS LOOP_TRIPCOUNT min = 307200 max = 307200
@@ -30,13 +30,19 @@ main_loop:
         dot_data_type in_data2 = dot_data_type(in_packet2.data);
 
         dot_data_type mul = in_data1*in_data2;
-        res += mul;
+        res[i % DOT_PPBUFF_SIZE] += mul;
 
         // if (in_packet.last == 1)
         //     break;
     }
 
-    result = float(res);
+    dot_data_type res2 = dot_data_type(0.0f);
+
+    final_loop:
+    for(int i = 0; i < DOT_PPBUFF_SIZE; i++)
+        res2 += res[i];
+
+    result = float(res2);
 
     return 0;
 }
