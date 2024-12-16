fix: remove normalization from nmist example, re-trained and updated the weights

python/depth_estimation.py
@@ -133,28 +133,28 @@ class SimpleDepthEstimationNet(nn.Module):
 class UNetDepthEstimator(nn.Module):
     def __init__(self, n_channels=3, n_classes=1):
         super(UNetDepthEstimator, self).__init__()
-        self.inc = DoubleConv(n_channels, 16)
-        self.down1 = Down(16, 32)
-        self.down2 = Down(32, 64)
-        #self.down3 = Down(256, 512)
-        #self.down4 = Down(512, 512)
+        self.inc = DoubleConv(n_channels, 8)
+        self.down1 = Down(8, 16)
+        self.down2 = Down(16, 32)
+        self.down3 = Down(32, 64)
+        self.down4 = Down(64, 128)
 
-        #self.up1 = Up(512, 256)
-        #self.up2 = Up(256, 128)
-        self.up3 = Up(64, 32)
-        self.up4 = Up(32, 16)
-        self.outc = nn.Conv2d(16, n_classes, kernel_size=1)
+        self.up1 = Up(128, 64)
+        self.up2 = Up(64, 32)
+        self.up3 = Up(32, 16)
+        self.up4 = Up(16, 8)
+        self.outc = nn.Conv2d(8, n_classes, kernel_size=1)
 
     def forward(self, x):
         x1 = self.inc(x)    # 16
         x2 = self.down1(x1) # 32
         x3 = self.down2(x2) # 64
-        #x4 = self.down3(x3) # 512
-        #x5 = self.down4(x4) # 512
+        x4 = self.down3(x3) # 512
+        x5 = self.down4(x4) # 512
 
-        #x = self.up1(x5, x4)
-        #x = self.up2(x, x3)
-        x = self.up3(x3, x2)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
         x = self.up4(x, x1)
         logits = self.outc(x)
         return logits
@@ -282,7 +282,9 @@ if __name__ == '__main__':
     ])
 
     depth_transform = T.Compose([
-        T.Resize((240, 320))
+        T.Resize((240, 320)),
+        #T.ToTensor(), 
+        #T.Normalize(mean=[2.0],std=[1.0])
     ])
 
     train_dataset = NYUMatDataset(images[train_indices], depths[train_indices],
@@ -290,9 +292,9 @@ if __name__ == '__main__':
     val_dataset = NYUMatDataset(images[val_indices], depths[val_indices],
                                 transform=rgb_transform, depth_transform=depth_transform)
 
-    train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=4)
-    val_loader = DataLoader(val_dataset, batch_size=4, shuffle=False, num_workers=4)
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=4)
 
     #model = SimpleDepthEstimationNet()
     model = UNetDepthEstimator()
-    train_depth_estimator(model, train_loader, val_loader, epochs=100, lr=1e-3, device='cpu')
+    train_depth_estimator(model, train_loader, val_loader, epochs=100, lr=1e-3, device='cuda')